fea38d95bc0c0cd5928958c8ee40169742d5c200
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-04-08 16:07:55 -0700

    darkmode enabled

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,24 +1,57 @@
-import { useLocalStorage } from './useLocalStorage';
-import { useEffect } from 'react';
+// import { useLocalStorage } from './useLocalStorage';
+// import { useEffect } from 'react';
+// import useLocalStorage from './useLocalStorage'
 
-export const useDarkMode = (key, initialValue) => {
-    // capture the values inside of useLocalStorage
-    const [values, setValues] = useLocalStorage('dark-mode')
+// export const useDarkMode = (key, initialValue) => {
+//     // capture the values inside of useLocalStorage
+//     const [darkmode, setDarkMode] = useLocalStorage('dark-mode', initialValue)
+//     //const [darkmode, setDarkMode] = useState('dark-mode')
 
 
-    useEffect(() => {
+//     useEffect(() => {
 
-  // do the logic to check for the value in localStorage
-    // if it's there, return that value
-    // else, return the initialValue from the parameters
-        if (localStorage.values === true) {
-            document.body.classList.add('dark-mode')
-          } else {
-            document.body.classList.remove('dark-mode')
-          }
-    })
-        // WHAT IS THE SETTING FUNCTION
-return [values, setValues]
+//   // do the logic to check for the value in localStorage
+//     // if it's there, return that value
+//     // else, return the initialValue from the parameters
+//         if (localStorage.values === true) {
+//             document.body.classList.add('dark-mode')
+//             setDarkMode({
+//               ...darkmode
+  
+//             })
+//           } else {
+//             document.body.classList.remove('dark-mode')
+
+//           }
+
+
+
+
+
+//     })
+      
+
+
+
+// return [darkmode, setDarkMode]
+
+// }
+
+import { useEffect } from 'react'
+import useLocalStorage from './useLocalStorage'
+
+export const useDarkMode = (initialValue) => {
+
+  const [ darkMode, setDarkMode] = useLocalStorage('dark-mode', initialValue);
+
+  useEffect(() => {
+
+    (darkMode) ? document.body.classList.add('dark-mode')
+      : document.body.classList.remove('dark-mode')
+
+  },[darkMode])
+  
+
+  return [darkMode, setDarkMode];
 
 }
-
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,30 +1,56 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export const useLocalStorage = (key, initalValue) => {
-    //GOAL: build this to be able to replace "useState" anywhere we want to persist any state data to localStorage
-    //setup a state property
-    //check to see if value is in local storage
-    //if not, set the value to localStorage
+// const useLocalStorage = (key, initalValue) => {
+//     //GOAL: build this to be able to replace "useState" anywhere we want to persist any state data to localStorage
+//     //setup a state property
+//     //check to see if value is in local storage
+//     //if not, set the value to localStorage
 
-    const [storedValue, setStoredValue] = useState(() =>{
-        //logic to check for the value
-        //get from local storage key 
-        const item = window.localStorage.getItem(key)
-        //Parse and return stored json or, if undefined, return initialValue
-        return item ? JSON.parse(item): initalValue;
-    })
+//     const [storedValue, setStoredValue] = useState(() =>{
+//         //logic to check for the value
+//         //get from local storage key 
+//         const item = window.localStorage.getItem(key)
+//         //Parse and return stored json or, if undefined, return initialValue
+//         return item ? JSON.parse(item): initalValue;
+//     })
 
 
 
 
     
-    const setValue = value => {
-        //save state
-        setStoredValue(value)
-        //Save to local storage
-        window.localStorage.setItem(key, JSON.stringify(value))
-    }
+//     const setValue = value => {
+//         //save state
+//         setStoredValue(value)
+//         //Save to local storage
+//         window.localStorage.setItem(key, JSON.stringify(value))
+//     }
 
 
-    return [storedValue] // returning state
+//     return [storedValue, setValue] 
+// }
+// export default useLocalStorage
+
+
+import { useState } from "react";
+
+const useLocalStorage = ( key, initialValue ) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    // Get item from localStorage by key
+    const item = window.localStorage.getItem(key);
+    // If item is in local storage, parse and return stored JSON.
+    // if undefined, return the initialValue
+    return item ? JSON.parse(item) : initialValue;
+  });  
+
+  const setValue = (value) => {
+    // Set state
+    setStoredValue(value);
+    // Save to local storage
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setValue];
 }
+
+export default useLocalStorage;
+
